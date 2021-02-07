0377e429b136dd286d0f6fde185c563903fbafbd
Biruk <mengi024@morris.umn.edu>
2021-02-07 17:45:35 -0600

    Added notification feature
    Added a `notify me` button to the page. Implemented outputting for components.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,7 +10,11 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share(): void {
+  share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
